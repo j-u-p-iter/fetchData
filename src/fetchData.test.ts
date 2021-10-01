1f51cb879f366f313d1fdfa82a1a82f9a29fce58
Kirill <pn79122821224@gmail.com>
2021-10-01 10:10:25 +0500

    feat(fetchData): initial commit

---
+++ src/fetchData.test.ts
@@ -0,0 +1,234 @@
+import nock from "nock";
+import fetch, { Request } from "node-fetch";
+import { fetchData } from ".";
+
+describe("fetchData", () => {
+  describe("shortcut methods", () => {
+    let requestOptions;
+
+    beforeAll(() => {
+      globalThis.fetch = fetch as any;
+      globalThis.Request = Request as any;
+
+      requestOptions = {
+        headers: {
+          Authentication: "secret"
+        }
+      };
+    });
+
+    describe("GET request", () => {
+      /**
+       * Check that the get wrapper around native fetch method::
+       *   - sends correct options (by checking sending headers);
+       *   - sends request to correct url
+       *   - resolves with correct result
+       */
+      it("is sent properly", async () => {
+        nock("http://some-url.com", { reqheaders: requestOptions.headers })
+          .get("/products")
+          .reply(200, { results: [{ id: 1 }, { id: 2 }] });
+
+        const result = await fetchData.get(
+          "http://some-url.com/products",
+          requestOptions
+        );
+
+        expect(result.status).toBe(200);
+        expect(result.data).toEqual({
+          results: [{ id: 1 }, { id: 2 }]
+        });
+      });
+    });
+
+    /**
+     * We don't test "with body", "without body" cases here as we do for the POST
+     *   request, cause PUT is always sent with the "body".
+     *
+     */
+    describe("PUT request", () => {
+      it("is sent properly", async () => {
+        nock("http://some-url.com", {
+          reqheaders: { "content-type": "application/json;charset=utf-8" }
+        })
+          .put("/products", { title: "new title" })
+          .reply(200, { results: [{ id: 1 }, { id: 2 }] });
+
+        const result = await fetchData.put("http://some-url.com/products", {
+          body: { title: "new title" }
+        });
+
+        expect(result.status).toBe(200);
+        expect(result.data).toEqual({
+          results: [{ id: 1 }, { id: 2 }]
+        });
+      });
+    });
+
+    /**
+     * We don't test "with body", "without body" cases here as we do for the POST
+     *   request, cause PATCH is always sent with the "body".
+     *
+     */
+    describe("PATCH request", () => {
+      it("is sent properly", async () => {
+        nock("http://some-url.com", {
+          reqheaders: { "content-type": "application/json;charset=utf-8" }
+        })
+          .patch("/products", { title: "new title" })
+          .reply(200, { results: [{ id: 1 }, { id: 2 }] });
+
+        const result = await fetchData.patch("http://some-url.com/products", {
+          body: { title: "new title" }
+        });
+
+        expect(result.status).toBe(200);
+        expect(result.data).toEqual({
+          results: [{ id: 1 }, { id: 2 }]
+        });
+      });
+    });
+
+    describe("POST request", () => {
+      let dataToSend;
+
+      beforeAll(() => {
+        dataToSend = {
+          title: "some title",
+          description: "some description"
+        };
+      });
+
+      it("sends post request properly", async () => {
+        nock("http://some-url.com", { reqheaders: requestOptions.headers })
+          .post("/products", dataToSend)
+          .reply(201, { results: [{ id: 1 }, { id: 2 }] });
+
+        const result = await fetchData.post("http://some-url.com/products", {
+          ...requestOptions,
+          body: dataToSend
+        });
+
+        expect(result.status).toBe(201);
+        expect(result.data).toEqual({
+          results: [{ id: 1 }, { id: 2 }]
+        });
+      });
+
+      describe("with body", () => {
+        it("sends content-type header as application/json", async () => {
+          nock("http://some-url.com", {
+            reqheaders: { "content-type": "application/json;charset=utf-8" }
+          })
+            .post("/products", dataToSend)
+            .reply(200, {});
+
+          await fetchData.post("http://some-url.com/products", {
+            body: dataToSend
+          });
+        });
+      });
+
+      describe("without body", () => {
+        it("does not send content-type header without sending body", async () => {
+          nock("http://some-url.com", { badheaders: ["content-type"] })
+            .post("/products")
+            .reply(200, {});
+
+          await fetchData.post("http://some-url.com/products");
+        });
+      });
+    });
+  });
+
+  /**
+   * TODO: find the way to test blob, formData and arrayBuffer
+   *   resolving methods.
+   *
+   */
+  describe("type option", () => {
+    describe("by default", () => {
+      it("resolves body as json", async () => {
+        nock("http://some-url.com")
+          .post("/products")
+          .reply(200, { key: "value", oneMoreKey: "oneMoreValue" });
+
+        const response = await fetchData.post("http://some-url.com/products");
+
+        expect(response.data).toEqual({
+          key: "value",
+          oneMoreKey: "oneMoreValue"
+        });
+      });
+    });
+
+    describe('with "text" value', () => {
+      it("resolves body as text", async () => {
+        nock("http://some-url.com")
+          .post("/products")
+          .reply(200, "Hello!");
+
+        const response = await fetchData.post("http://some-url.com/products", {
+          type: "text"
+        });
+
+        expect(response.data).toBe("Hello!");
+      });
+    });
+
+    /**
+     * Using duck typing to check that instance
+     *   belongs to Blob class.
+     *
+     */
+    describe('with "blob" value', () => {
+      it("resolves body as blob", async () => {
+        nock("http://some-url.com")
+          .post("/products")
+          .reply(200, {});
+
+        const response = await fetchData.post("http://some-url.com/products", {
+          type: "blob"
+        });
+
+        expect(response.data.constructor.name).toBe("Blob");
+        expect(typeof response.data.text).toBe("function");
+        expect(typeof response.data.arrayBuffer).toBe("function");
+        expect(typeof response.data.stream).toBe("function");
+      });
+    });
+
+    /**
+     * Using duck typing to check that instance
+     *   belongs to ArrayBuffer class.
+     *
+     */
+    describe('with "arrayBuffer" value', () => {
+      it("resolves body as text", async () => {
+        nock("http://some-url.com")
+          .post("/products")
+          .reply(200, {});
+
+        const response = await fetchData.post("http://some-url.com/products", {
+          type: "arrayBuffer"
+        });
+
+        expect(response.data.constructor.name).toEqual("ArrayBuffer");
+        expect(typeof response.data.byteLength).toBe("number");
+        expect(typeof response.data.slice).toBe("function");
+      });
+    });
+  });
+
+  describe("HTTP error", () => {
+    it("is thrown in case non successful response (non-2xx status)", async () => {
+      nock("http://some-url.com")
+        .get("/products")
+        .reply(400);
+
+      expect(fetchData.get("http://some-url.com/products")).rejects.toThrow(
+        "Request is not resolved successfully"
+      );
+    });
+  });
+});
---
+++ src/fetchData.ts
@@ -0,0 +1,170 @@
+import { CommonHttpError } from "@j.u.p.iter/custom-error";
+/**
+ * Handle known errors (HTTPError, TypeError)
+ *   and throw uknown unexpected errors.
+ */
+
+enum HTTPMethod {
+  GET = "get",
+  PUT = "put",
+  POST = "post",
+  PATCH = "patch",
+  DELETE = "delete"
+}
+
+enum ResponseBodyType {
+  JSON = "json",
+  BLOB = "blob",
+  TEXT = "text",
+  FORM_DATA = "formData",
+  ARRAY_BUFFER = "arrayBuffer"
+}
+
+interface FetchDataOptions extends RequestInit {
+  /**
+   * The "type" is used to set up the method you want
+   *   to call on the response intance to extract
+   *   the response's body. By default it's set up to the "json",
+   *   cause in the mafority of cases the consumer we'll
+   *   need to use the data as "json".
+   */
+  type?: ResponseBodyType;
+  data?: { [key: string]: any };
+}
+
+interface FetchDataResponse<T> {
+  status: number;
+  statusText: string;
+  data: T;
+}
+
+const isObject = input => typeof input === "object" && input !== null;
+
+function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+  return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
+}
+
+const DEFAULT_EXPECTED_DATA_TYPE = "json";
+const CONTENT_TYPE_HEADER = {
+  "Content-Type": "application/json;charset=utf-8"
+};
+const CONTENT_TYPE_HEADER_TITLE = Object.keys(CONTENT_TYPE_HEADER)[0];
+
+const optionsReducer = (options: any, httpMethod: HTTPMethod) => {
+  if (
+    httpMethod === HTTPMethod.POST ||
+    httpMethod === HTTPMethod.PUT ||
+    httpMethod === HTTPMethod.PATCH
+  ) {
+    /**
+     * If a request body is a string, the Content-Type header is set to
+     *  "text/plain;chartset=utf-8" by default.
+     *
+     * If options.body is an object we'll need to JSON.stringify it
+     *  before sending it to the serveri. So, in this case the "Content-Type"
+     *  header will be set up as "text/plain;charset=utf-8" by default.
+     *
+     * Because we send JSON to the server but not the plain text we need
+     *  to reassign value for the default "Content-Type"
+     *  header to "application/json;charset=utf-8".
+     *
+     */
+
+    if (
+      isObject(options.body) &&
+      !options.headers?.[CONTENT_TYPE_HEADER_TITLE]
+    ) {
+      options.headers = {
+        ...(options.headers || {}),
+        ...CONTENT_TYPE_HEADER
+      };
+    }
+
+    if (options.body) {
+      options.body = JSON.stringify(options.body);
+    }
+  }
+
+  return options;
+};
+
+/**
+ * Private _fetch method.
+ *
+ * Responsible for
+ *   1. taking request's url and request's options;
+ *   2. preparing request's options with help of "optionsReducer";
+ *   3. sending HTTP request;
+ *   4. resolving HTTP request.
+ *   5. returning back all response-related data.
+ *
+ */
+const internalFetch = async <T>(
+  url: string,
+  originalOptions: FetchDataOptions,
+  httpMethod: HTTPMethod
+): Promise<FetchDataResponse<T>> => {
+  const { type, ...options } = originalOptions;
+
+  /**
+   * Assign request to a variable to be able to pass it with CommonHttpError
+   */
+
+  const request = new Request(url, optionsReducer(options, httpMethod));
+
+  const response = await fetch(request);
+
+  /**
+   * Instead of resolving Promise in case of error status (as native Fetch API does),
+   *   we reject it
+   *
+   */
+  if (!response.ok) {
+    throw CommonHttpError(
+      "Request is not resolved successfully",
+      { code: response.status, response, request },
+      { context: "@j.u.p.iter/fetchData" }
+    );
+  }
+
+  /**
+   * In majority of cases we need to resolve data
+   *   as a json. So, we set up the "json" resolving method
+   *   as a default one.
+   */
+  const expectedDataType = type || DEFAULT_EXPECTED_DATA_TYPE;
+  const responseData = await response[expectedDataType]();
+
+  return {
+    status: response.status,
+    statusText: response.statusText,
+    data: responseData
+  };
+};
+
+/**
+ * Iterates through the enum to declare shortcut methods for all possible
+ *   types of HTTP requests in one single place without necessety to
+ *   duplicate things.
+ *
+ */
+export const fetchData = enumKeys(HTTPMethod).reduce(
+  (resultFetchData, httpMethod) => {
+    resultFetchData[HTTPMethod[httpMethod]] = <T = any>(
+      url: string,
+      options: FetchDataOptions = {}
+    ) => {
+      return internalFetch<T>(
+        url,
+        {
+          method: HTTPMethod[httpMethod],
+          ...options
+        },
+        HTTPMethod[httpMethod]
+      );
+    };
+
+    return resultFetchData;
+  },
+  {}
+) as any;
